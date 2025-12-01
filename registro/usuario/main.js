26390410052199d9a03629548d3ca5874252ee1d
enrique <enriquemantillamaldonado@hotmail.com>
2025-12-01 05:11:02 -0500

    header responsive

--- registro/usuario/main.js
+++ registro/usuario/main.js
@@ -1,6 +1,20 @@
 
+// Selecciona el botón de hamburguesa
+const btn = document.querySelector(".menu-toggle");
+
+// Selecciona el menú horizontal
+const menu = document.querySelector(".menu-horizontal");
 
 const activarSOS = document.getElementById("boton-sos");
+
+// Al hacer clic en el botón de hamburguesa
+if(btn && menu){
+  btn.addEventListener("click", () => {
+
+    menu.classList.toggle("active");
+  
+  });
+}
 
 if (activarSOS) {
     activarSOS.addEventListener("click", function(e) {
